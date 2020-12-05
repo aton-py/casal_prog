62b7d55d1d08f1a306e808616e57730af326adb0
Aton Bertini Dornfeld <68289323+aton-py@users.noreply.github.com>
2020-12-05 19:31:53 -0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,9 +12,9 @@ export default () => {
     <>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>{title || 'Gatsby Simplefolio'}</title>
+        <title>{title || 'Reis e Bertini'}</title>
         <html lang={lang || 'en'} />
-        <meta name="description" content={description || 'Gatsby Simplefolio'} />
+        <meta name="description" content={description || 'Desenvolvimento web & mobile'} />
       </Helmet>
       <App />
     </>
